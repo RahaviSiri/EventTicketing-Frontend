c433df01af6a86448e9eac6c23fc0e57c101f922
RahaviSiri <rahavi24siri@gmail.com>
2025-07-17 14:24:35 +0530

    Add sidebar,navbar and pages for organizers

---
+++ client-frontend/src/context/AppContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from "react";
+
+// Create the context
+export const AppContext = createContext();
+
+// Create the context Provider
+export const AppContextProvider = ({children}) => {
+
+    const [role,setRole] = useState("ORGANIZER");
+
+    const value = {
+        role,
+        setRole,
+
+    }
+
+    return ( 
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
