42b97f955b64f5033c56ee1d2bc85835cd5480a1
Shalikatkt <tktshalika@gmail.com>
2025-08-31 18:54:53 +0530

    sete up api

--- client-frontend/src/constants/colors.js
+++
@@ -1,8 +0,0 @@
-const colors = {
-  primary: "#8076a3",     // Purple-Mountains-Majesty
-  secondary: "#e0dbf3",   // Factory-Stone-Purple
-  accent: "#feb300"       // Sleuthe Yellow
-
-};
-
-export default colors;
